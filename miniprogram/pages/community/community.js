b1c5aeca4d843e5310df19d5edd600c11d23339e
jgyyz <1624180634@qq.com>
2020-03-18 16:47:27 +0800

    改造【宠物选择】模态框✔

--- miniprogram/pages/community/community.js
+++ miniprogram/pages/community/community.js
@@ -39,7 +39,8 @@ Page({
     momentList: [[]],
     topicList: [],
     topicSortType: "综合排序", // 排序方式
-    isPetSelected: false,
+    // isPetSelected: false,
+    isPetDialogOpen: false,
     animation: {},
     currentIndex: -1,
     petList: [],
@@ -167,60 +168,13 @@ Page({
       });
   },
 
-  // 抽屉动画
-  _animate(status) {
-    // 第1步：创建动画实例
-    const animation = wx.createAnimation({
-      duration: 200, //动画时长
-      timingFunction: "linear", //线性
-      delay: 0 //0则不延迟
-    });
-
-    // 第2步：这个动画实例赋给当前的动画实例
-    this.animation = animation;
-
-    // 第3步：执行第一组动画：Y轴偏移240px后(盒子高度是240px)，停
-    animation.translateY(240).step();
-
-    // 第4步：导出动画对象赋给数据对象储存
-    this.setData({
-      animation: animation.export()
-    });
-
-    // 第5步：设置定时器到指定时候后，执行第二组动画
-    setTimeout(
-      function() {
-        // 执行第二组动画：Y轴不偏移，停
-        animation.translateY(0).step();
-        // 给数据对象储存的第一组动画，更替为执行完第二组动画的动画对象
-        this.setData({
-          animation
-        });
-
-        //关闭
-        if (status == "close") {
-          this.setData({
-            isPetSelected: false
-          });
-        }
-      }.bind(this),
-      200
-    );
-
-    // 显示
-    if (status == "open") {
-      this.setData({
-        isPetSelected: true
-      });
-    }
-  },
-
   _setLoginShow() {
     this.setData({
       loginShow: true
     });
   },
 
+  // 选择宠物
   handleClick(event) {
     const {
       currentTarget: {
@@ -255,10 +209,9 @@ Page({
         });
       } else {
         this.setData({
-          // isPetSelected: true,
+          isPetDialogOpen: true,
           petList
         });
-        this._animate("open");
       }
     } else {
       this._setLoginShow();
@@ -272,18 +225,17 @@ Page({
       wx.navigateTo({
         url: `../moment/moment-edit-box/moment-edit-box?nickName=${nickName}&avatarUrl=${avatarUrl}&index=${currentIndex}`
       });
-      this._animate("close");
+      this.closeDialog();
     } else {
       return;
     }
   },
 
-  handleCancel() {
+  closeDialog() {
     this.setData({
-      isPetSelected: false,
+      isPetDialogOpen: false,
       currentIndex: -1
     });
-    this._animate("close");
   },
 
   /**
